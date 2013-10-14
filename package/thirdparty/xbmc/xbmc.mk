e1fd1db29850ba66c057f46c28ca703e7db015d6
Ilia Mogilevsky <ilia@mogilevsky.com.au>
2013-10-15 06:45:49 +1100

    BUMP xbmc - upnp changes

--- package/thirdparty/xbmc/xbmc.mk
+++ package/thirdparty/xbmc/xbmc.mk
@@ -4,7 +4,7 @@
 #
 #################################################################################
 
-XBMC_VERSION = 201e9a714956f0ea5f00e09e5020f65c683098cb
+XBMC_VERSION = dbaefe9a92954005c3c5d50bfa2f24a69a0725bd
 XBMC_SITE_METHOD = git
 XBMC_SITE = git://github.com/Ilia/xbmc.git
 XBMC_INSTALL_STAGING = YES
