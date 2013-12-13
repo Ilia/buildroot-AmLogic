0e36339bf3f0e697337b3a632949c054ca6f25a3
Ilia Mogilevsky <ilia@mogilevsky.com.au>
2013-12-14 08:14:41 +1100

    BUMP xbmc - airplay fix

--- package/thirdparty/xbmc/xbmc.mk
+++ package/thirdparty/xbmc/xbmc.mk
@@ -4,7 +4,7 @@
 #
 #################################################################################
 
-XBMC_VERSION = 37c57eec30a99c935446fe0b7ff2d8c693b63f9b
+XBMC_VERSION = 885a12c80771aea793e03abe2fd157545cb09812
 XBMC_SITE_METHOD = git
 XBMC_SITE = git://github.com/Ilia/xbmc.git
 XBMC_INSTALL_STAGING = YES
